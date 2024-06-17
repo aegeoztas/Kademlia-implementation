f21afe7f353c3a215e284052c347f49627f5ba6e
Loris <loris.verga@protonmail.com>
2024-06-17 21:49:52 +0200

    adv handler

--- handler.py
+++ handler.py
@@ -3,6 +3,7 @@ import argparse
 import asyncio
 from socket import AF_INET
 from LocalNode import LocalNode
+from kademlia import NodeTuple
 from network.messages import *
 from util import *
 from network.Kademlia import GET, PUT, SUCCESS, FAILURE
@@ -13,7 +14,11 @@ DHT_PORT = 7401
 
 SIZE_FIELD_SIZE = 2
 MESSAGE_TYPE_FIELD_SIZE = 2
-KEY_SIZE = 256
+KEY_FIELD_SIZE = 256 / 8
+IP_FIELD_SIZE = 32 / 8
+PORT_FIELD_SIZE = 16 / 8
+
+NB_OF_CLOSEST_PEERS = 4
 
 
 class Handler:
@@ -39,17 +44,20 @@ class Handler:
                 case DHT_PUT.value:
                     return_status = await handle_put()
                 case DHT_GET.value:
-                    key = int.from_bytes(struct.unpack("B" * KEY_SIZE, buf[5: 5 + KEY_SIZE -1]))
-                    return_status = await self.handle_get(reader, writer, key)
+                    key = int.from_bytes(struct.unpack("B" * KEY_FIELD_SIZE, body[0: KEY_FIELD_SIZE - 1]))
+                    return_status = await self.handle_get_request(reader, writer, key)
                 case DHT_FIND_NODE.value:
-                    return_status = await handle_find_node()
+                    key = int.from_bytes(struct.unpack("B" * KEY_FIELD_SIZE, body[0: KEY_FIELD_SIZE - 1]),
+                                         byteorder="big")
+                    return_status = await self.handle_find_nodes_request(reader, writer, key)
                 case DHT_STORE.value:
                     ttl = int(struct.unpack(">H", body[0:1]))
                     replication = int(struct.unpack(">B", body[2]))
-                    key = int.from_bytes(struct.unpack("B" * KEY_SIZE, buf[4: 4 + KEY_SIZE - 1]), byteorder="big")
-                    value_field_size = len(body) - KEY_SIZE - 4
-                    value = struct.unpack("B" * value_field_size, buf[4 + KEY_SIZE:])
-                    return_status = await self.handle_store(ttl, key, value)
+                    key = int.from_bytes(struct.unpack("B" * KEY_FIELD_SIZE, body[4: 4 + KEY_FIELD_SIZE - 1]),
+                                         byteorder="big")
+                    value_field_size = len(body) - KEY_FIELD_SIZE - 4
+                    value = struct.unpack("B" * value_field_size, body[4 + KEY_FIELD_SIZE:])
+                    return_status = await self.handle_store_request(ttl, key, value)
                 case _:
                     await bad_packet(reader, writer,
                                      f"Unknown message type {message_type} received",
@@ -83,7 +91,7 @@ class Handler:
 
         return True
 
-    async def handle_store(self, ttl: int, key: int, value: bytes):
+    async def handle_store_request(self, ttl: int, key: int, value: bytes):
         """
         This function handles a store message. If this function is called, this node has been designated to store a value.
         It must then store the value in its storage.
@@ -97,8 +105,7 @@ class Handler:
         self.local_node.local_hash_table.put(key, value, ttl)
         return True
 
-
-    async def handle_get(self, reader, writer, key: int):
+    async def handle_get_request(self, reader, writer, key: int):
         """
         This method handles a get message. It the local node contains the data,
         it will simply return it. If not, it will try to get from the network.
@@ -107,25 +114,116 @@ class Handler:
         :param key: The key associated to the data.
         :return: True if the operation was successful.
         """
+        # Get the address of the remote peer
+        remote_address, remote_port = writer.get_extra_info("socket").getpeername()
+
+        # Check if the value is in the local storage
         value = self.local_node.local_hash_table.get(key)
-        if value is not None: # TODO check syntax
+        if value is not None:
             # return value to the requester
             # Define the DHT Success message
             message_size = int(SIZE_FIELD_SIZE + MESSAGE_TYPE_FIELD_SIZE
-                               + KEY_SIZE + len(value))
+                               + KEY_FIELD_SIZE + len(value))
             response = struct.pack(">HH", message_size,
                                    DHT_SUCCESS)
-            response += key
+            response += struct.pack("B" * KEY_FIELD_SIZE, key)
             response += value
-            # Send
+            # Send response
+            try:
+                writer.write(response)
+                await writer.drain()
+            except Exception as e:
+                print(f"[-] Failed to send DHT_SUCCESS {e}")
+                await bad_packet(reader, writer, data=response)
+                return False
+
+            print(f"[+] {remote_address}:{remote_port} <<< DHT_SUCCESS")
+            return True
 
         else:
-            # Query value
+            # Retrieve the value in the network
+            # Find the closest nodes to the value
+            pass
 
         return
 
-    async def handle_put():
+    async def handle_find_nodes_request(self, reader, writer, key: int):
+        """
+        This function returns to the sender the k closest nodes to a key located in its routing table.
+        :param key:The key for which we want to find the closest nodes to this key.
+        :param reader: The reader of the socket
+        :param writer: The writer of the socket
+        :return: True if the operation was successful.
+        """
+
+        # Get the address of the remote peer
+        remote_address, remote_port = writer.get_extra_info("socket").getpeername()
+
+        # Getting the closest nodes
+        closest_nodes: list[NodeTuple] = self.local_node.routing_table.get_nearest_peers(key, NB_OF_CLOSEST_PEERS)
+        nb_of_nodes_found = len(closest_nodes)
+
+        NUMBER_OF_NODES_FIELD_SIZE = 2
+
+        # Constructing response message
+        message_size = int(SIZE_FIELD_SIZE
+                           + MESSAGE_TYPE_FIELD_SIZE
+                           + NUMBER_OF_NODES_FIELD_SIZE
+                           + (KEY_FIELD_SIZE + IP_FIELD_SIZE + PORT_FIELD_SIZE) * nb_of_nodes_found)
+        response = struct.pack(">HHH", message_size, DHT_FIND_NODE_RESP, nb_of_nodes_found)
+
+        # Add each node information to the message
+        for node in closest_nodes:
+            # Add node ID field
+            response += struct.pack("B" * KEY_FIELD_SIZE, node.node_id)
+            # Add IP field
+            ip_parts = map(int, node.ip_address.split())
+            response += struct.pack(">BBBB", *ip_parts)
+            # Add port field
+            response += struct.pack(">H", node.port)
+
+        # Send the response
+        try:
+            writer.write(response)
+            await writer.drain()
+        except Exception as e:
+            print(f"[-] Failed to send DHT_FIND_NODE_RESP {e}")
+            await bad_packet(reader, writer, data=response)
+            return False
+        print(f"[+] {remote_address}:{remote_port} <<< DHT_SUCCESS")
+        return True
+
+    async def handle_put_request(self):
         return
 
-    async def handle_find_node():
-        return
+
+    #---------------------------------------------------------------------------------------------------------
+    # Async functions that are not handler of received messages:
+    #---------------------------------------------------------------------------------------------------------
+    async def find_closest_nodes_in_network(self, key: int):
+
+        closest_nodes : list[NodeTuple] = self.local_node.routing_table.get_nearest_peers(key, NB_OF_CLOSEST_PEERS)
+        nodes_to_query : list[NodeTuple] = list(closest_nodes)
+
+        # Definition of query message
+        message_size = SIZE_FIELD_SIZE + MESSAGE_TYPE_FIELD_SIZE + KEY_FIELD_SIZE
+        query = struct.pack(">HH", message_size, DHT_FIND_NODE)
+        # TODO update pack thing below
+        query += struct.pack(">", key)
+
+
+        done = False
+        # done = list to query is empty
+        # add to list to query only when nodes getted are closer that to node quried
+        while not done:
+            pass
+        for node in nodes_to_query:
+            # query node
+            # make connection
+            # do it parallel and asynchronously
+            # shit it will probably be complicated
+
+            # list = result
+            # TODO add filter to then sending message as well ?
+            # filter_result : keep only nodes that are closer than the node itself
+            pass
