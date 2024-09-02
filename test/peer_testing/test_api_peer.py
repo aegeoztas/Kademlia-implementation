c7a48a93980d0dc4e5b42327316cfa3f1a0b9773
Ege Oztas <egeoztas66@gmail.com>
2024-09-02 19:22:36 +0200

    added first test for api peer messaging but it is not working.

--- test/peer_testing/test_api_peer.py
+++ test/peer_testing/test_api_peer.py
@@ -2,14 +2,14 @@ import asyncio
 import struct
 import random
 import subprocess
+from asyncio import StreamReader, StreamWriter
 import os
 import time
 
 import requests
-import pytest
 import configparser
 from cryptography.hazmat.primitives.asymmetric import rsa
-from test.dht_api_client import send_message
+
 
 """
 this test is for testing p2p interface of the dht implementation
@@ -18,29 +18,58 @@ config = configparser.ConfigParser()
 
 # Read the config.ini file
 config.read('config.ini')
+# Global variable
+SIZE_FIELD_SIZE =2
+MESSAGE_TYPE_FIELD_SIZE =2
+"""
+this is not working. 
+
+"""
 
 
-# Access values
+async def send_message(message_type: int, payload: bytes, host: str, port: int):
 
-async def gen_public_key():
-    private_key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=2048,
-    )
-    return private_key.public_key()
+    # Declaration of reader and writer
+    reader: StreamReader
+    writer: StreamWriter
+    full_response = None
+    reader, writer = await asyncio.open_connection(host, port)
+    try:
+        # Establish connection to server
 
 
-@pytest.fixture(scope="session", autouse=True)
-def make_dht_docker():
-    """
-    Build and run an instance of dht-5 peer instance for testing
-    """
-    script_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../run_docker.sh"))
-    subprocess.run([script_path], check=True, shell=True)
-    time.sleep(15)# wait for docker to load
-    yield
+        # Determine the size of the message and create the size field
+        size_of_message: int = SIZE_FIELD_SIZE  + MESSAGE_TYPE_FIELD_SIZE + len(payload) # Total size including the size field
 
-@pytest.mark.asyncio
+        size_field: bytes = struct.pack(">H", size_of_message)
+        message_type_field: bytes = struct.pack(">H", message_type)
+
+
+        # Create full message
+        full_message: bytes = size_field + message_type_field + payload
+
+        # Send the full message to the server
+        writer.write(full_message)
+        await writer.drain()  # Ensure the message is sent
+
+        # Await response from the server
+        response_size_bytes = await reader.read(SIZE_FIELD_SIZE)
+        response_size: int= struct.unpack(">H", response_size_bytes)[0]
+        response = await reader.read(response_size)
+        full_response = response_size_bytes + response
+
+
+
+    except Exception as e:
+        print(f"Error communicating with server: {e}")
+
+    finally:
+        writer.close()
+        await writer.wait_closed()
+        return full_response
+
+
+
 async def test_put():
     api_host = "127.0.0.1"
     api_port = 8889
@@ -58,3 +87,14 @@ async def test_put():
     result =  await send_message(message_type=message_type, payload=payload, host=api_host, port=api_port)
     print(result)
 
+
+if __name__ == "__main__":
+    # Example usage
+    # message_type = 56 # Example message type
+    # payload = b'Hello, Server!'  # Example payload
+    #
+    # asyncio.run(send_message(message_type, payload, '127.0.0.1', 8888))
+
+    remote_ip = "127.0.0.1"
+    remote_port = 8888
+    asyncio.run(tput())
